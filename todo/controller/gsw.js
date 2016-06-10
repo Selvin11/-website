ca501bc2128a4b6f25ea763a5deced7b3c26764d
Selvin11 <528370630@qq.com>
2016-06-10 18:18:32 +0800

    datetime

--- todo/controller/gsw.js
+++ todo/controller/gsw.js
@@ -1,39 +1,81 @@
-
-
 var todo = Todo.createNew();
 var dl = $('.categories')[0];
+var leftList = $('.left-list')[0];
 var midList = $('.mid-list')[0];
 var rightList = $('.right-list')[0];
+
 // 设定初始高度
-todo.curHeight($('.left-list')[0]);
-todo.curHeight($('.mid-list')[0]);
+curHeight(leftList,42);
+curHeight(midList,42);
 // 设置窗口变化时，高度同步变化
 window.onresize = function () {
-	todo.curHeight($('.left-list')[0]);
-	todo.curHeight($('.mid-list')[0]);
+	curHeight(leftList,42);
+	curHeight(midList,42);
 }
+function curHeight (elem,reduceHeight) {
+	 elem.style.height = window.innerHeight - reduceHeight + 'px';
+}
+
 var add1 = $('.add-list')[0];
 var add2 = $('.add-list')[1];
 add1.onclick = function () {
 	// 新增分类-----------------------------------目录
 	mkdir('目录 : ', dl , 'dt' , '目录 : ');
+	// overShowDel(dl);
 	// 新增分类------------------------------------文件
 	// var childs = dl.children; 这里缓存之后会导致子节点不能实时更新，会增加遍历次数
 	// 在增加目录节点之后第一次遍历
 	for (var i = 0; i < dl.children.length; i++) {
 		// 增加目录下的文件
 		if (dl.children[i].nodeName.toLowerCase() === 'dt') {
+
 			dl.children[i].onclick = function (e) {
 			// console.log(e.target);
 			var that = this;
-			mkfile(that,'文件 : ',dl,that,'文件 : ');//同时也新增了task div 	
-
-			// mid-list区域内容
-			// 获取对应的task-----------------------  div
-			// dd 点击增加task div 
-			getTask(dl.children);
-							
-
+			// 使得dt 和 dd 建立联系，届时一起删除
+			mkfile('文件 : ', dl , that ,'文件 : ');//同时也新增了task div 	
+ 			overShowDel(dl);
+			// mid-list区域内容====================================================
+			clickShowDiv(dl,'dd',midList,2);
+	
+			
+				add2.onclick = function () {	
+					// 获取对应的task-----------------------  div
+					// var getDiv(midList , 2) = getDiv(midList , 2);
+					// var getDiv(rightList , 0) = getDiv(rightList , 0);
+					
+					if(getDiv(midList , 2)){
+						// midList 增加描述内容
+						// mid 中部的时间标题
+						var midDate = mkMidDate(getDiv(midList , 2));
+						var result = mkMidCon(getDiv(midList , 2));
+						midDate.className = result;
+						
+						// rightLis 增加标题、日期、富文本编辑区,还有一个包裹div
+						// 增加右边的包裹div
+						var rightWrap = addDiv(getDiv(rightList , 0),result);
+						showDiv(getDiv(rightList , 0),result,0);
+						mktit(rightWrap,result);
+						// 获取时间
+						var date = addDate(rightWrap);
+						// 获取富文本编辑区
+						var fwd = mkfwd(rightWrap);
+						clickShowDiv(getDiv(midList , 2),'p',getDiv(rightList , 0),0);
+						curHeight(fwd,126);
+						window.onresize = function () {
+							curHeight(fwd,126);
+						}
+						// 将右边时间赋值给中间的时间标题
+						var jq = jQuery.noConflict();
+						jq(".form_datetime")
+							.datetimepicker({format: 'yyyy-mm-dd hh:ii'})
+							.on('changeDate',function (e) {
+								var $that = jq(this);
+								console.log($that.val());
+								jq(midDate).html($that.val());
+							})
+					}			
+				}
 			// 阻止冒泡到目录层产生多余div
 			e.stopPropagation();
 			}//dt.onclick -------- end
@@ -41,142 +83,6 @@ add1.onclick = function () {
 	}// add1.onclick = null;	
 }
 // $('.add-list')[0].onclick ----------end
-
-
-
-
-
-// 找到文件对应的div
-function getTask(parentNode) {
-	for (var i = 0; i < parentNode.length; i++) {
-		if (parentNode[i].nodeName.toLowerCase() === 'dd'){
-			parentNode[i].onclick = function (e) {
-				// 根据dd的className获取task下的div
-				var id = this.className;
-	
-				toggleTask(midList.children, id, 2);
-				
-				e.stopPropagation();
-
-			}
-		}
-	}
-}
-
-
-// 任务栏中的切换
-function toggleTask(targetNode,id,num,func) {
-	for (var i = num; i < targetNode.length; i++) {
-		if (targetNode[i].className === 'task' + id ) {
-			targetNode[i].style.display = 'block';
-			var task = targetNode[i];
-			console.log(task);					
-			// left-list 向 mid-list 传递成功
-			// 在点击增加任务的同时，将rightList内容生成
-			add2.onclick = function () {
-				// 在对应的task栏中生成对应的内容
-				// 获取add2点击时的内容
-				var result = mkTaskCon(task);
-				console.log(result);
-
-				// rightList --------------------------------------- div
-				// 在创建之初，使其能够切换显示==================
-				toggleFwb(rightList,result);
-				var parent = addDiv (task,rightList,result,'fwb');
-				
-				// 添加mid-list的标题和日期
-				mktit(parent,result);
-				addDate(parent);
-				mkfwd(parent);
-				
-				// 给task栏下的p元素增加点击切换====================
-				for (var i = 0; i < task.children.length; i++) {
-					task.children[i].onclick = function () {
-						var that = this;
-						toggleFwb(rightList,that.innerHTML)
-					}
-				}
-			}
-
-		}else{
-			targetNode[i].style.display = 'none';
-		}
-	}
-}
-
-// rightList  切换显示函数
-function toggleFwb(targetNode,result) {
-	for (var i = 0; i < targetNode.children.length; i++) {
-		if (targetNode.children[i].className === 'fwb' + result) {
-			targetNode.children[i].style.display = 'block';
-		}else{
-			targetNode.children[i].style.display = 'none';
-		}
-	}
-}
-
-
-
-// 新增富文本选区---------rightList div
-// 已经采用addDiv() className === 'fwb' + result
-// 新增富文本选区---------标题
-function mktit(parentNode,result) {
-	var p = document.createElement('p');
-	p.innerHTML = result;
-	for (var i = 0; i < parentNode.children.length ;i++) {
-		if(parentNode.children[i].nodeName.toLowerCase() === 'p' && parentNode.children[i].innerHTML === result){
-			return;
-		}
-	}
-	parentNode.appendChild(p);
-}
-// 新增富文本选区---------富文本
-function mkfwd(parentNode) {
-	var div = document.createElement('div');
-	div.contentEditable = true;
-	parentNode.appendChild(div);
-}
-// 新增任务弹框逻辑及内容
-function mkTaskCon(parentNode) {
-	var p = document.createElement('p');
-	var result = prompt();
-	// 限制相同输出
-	for (var i = 0; i < parentNode.children.length ;i++) {
-		if(parentNode.children[i].nodeName.toLowerCase() === 'p' && parentNode.children[i].innerHTML === result){
-			return;
-		}
-	}
-	if (result !== ''&& result !== null) {
-		p.innerHTML = result;
-		parentNode.appendChild(p);
-		return result;
-	}
-}
-// 任务日期
-function addDate(parentNode){
-	var now = new Date();
-	var time = now.getFullYear() + ' 年 ' + (now.getMonth()+1) + ' 月 ' + now.getDate() + '日';
-	for (var i = 0; i < parentNode.children.length ;i++) {
-		if(parentNode.children[i].nodeName.toLowerCase() === 'h3' && parentNode.children[i].innerHTML === time){
-			return;
-		}
-	}
-	var h3 = document.createElement('h3');
-	var span = document.createElement('span');
-	span.innerHTML = '任务日期 : ';
-	// h3.innerHTML = time;
-	h3.appendChild(span);
-	parentNode.appendChild(h3);
-}
-
-
-
-
-
-
-
-
-
 
 
 
@@ -193,23 +99,23 @@ function mkdir(title,parentNode,childNode,describe) {
 	}
 }
 // 新建文件
-function mkfile(that,title,parentNode,target,describe) {
+function mkfile(title,parentNode,target,describe) {
 	var result = prompt(title);
 	if (result !== ''&& result !== null) {
 		addDd(result,parentNode,target,describe)
-		
-		// 同时新建一个对应的任务栏
-		// 对目录层同名进行查重处理
-		if (that.className !== result) {
-			addDiv(parentNode,midList,result,'task');
-		}
-		//result === ID  className
+		// 同时新建一个对应的任务栏,dd 的 result className 本来就是唯一的
+		// 新建div的同时，设置其display为block, 其余div display:none
+		addDiv(midList,result);
+		showDiv(midList,result,2);
+		// 同时新建一个对应的right div , 写字栏
+		addDiv(rightList,result);
+		showDiv(rightList,result,0);
 		// 增加删除功能
 		removeDs(parentNode,result);
 	}
 }
 // 创建dt
-function addDt (title,parentNode,describe) {
+function addDt (result,parentNode,describe) {
 	// 创建目录dt
 	var dt = document.createElement('dt');
 	// 目录名
@@ -219,26 +125,24 @@ function addDt (title,parentNode,describe) {
 	var ds = parentNode.children;
 	// 遍历子节点，看是否有重复标记
 	for (var k = 0; k < ds.length; k++) {
-		if(ds[k].className === title){
+		if(ds[k].className === result){
 			return;
 		}
 	}
-	dt.innerHTML = title;
+	dt.innerHTML = result;
 	parentNode.appendChild(dt);
-	for (var i = 0; i < ds.length; i++) {
 		// 标记，附上唯一classname
-		dt.className = title;
+		dt.className = result;
 		span1.innerHTML = describe;
 		dt.insertBefore(span1,dt.firstChild);
 		// 暂时性的删除设置
-		span2.innerHTML = ' X ';
+		// span2.innerHTML = ' X ';
 		span2.className = 'del';
-		dt.appendChild(span2);
-	}
-	
+		span2.style.display = 'none';
+		dt.appendChild(span2);	
 };
 // 创建dd
-function addDd (title,parentNode,target,describe) {
+function addDd (result,parentNode,target,describe) {
 	// 创建文件dd
 	var dd = document.createElement('dd');
 	// 目录名
@@ -247,87 +151,207 @@ function addDd (title,parentNode,target,describe) {
 	var span2 = document.createElement('span');
 	var ds = parentNode.children;
 	for (var k = 0; k < ds.length; k++) {
-		if(ds[k].className === title){
+		if(ds[k].className === result){
 			return;
 		}
 	}
-	dd.innerHTML = title;
+	dd.innerHTML = result;
 	insertAfter(dd,target);
-	for (var i = 0; i < ds.length; i++) {
+
 		// 标记
-		dd.className = title;
+		dd.className = result;
 		span1.innerHTML = describe;
 		dd.insertBefore(span1,dd.firstChild);
 		// 暂时性的删除设置
-		span2.innerHTML = ' X ';
+		// span2.innerHTML = ' X ';
 		span2.className = 'del';
+		span2.style.display = 'none';
 		dd.appendChild(span2);
-	}
 	
 };
 // 移除目录或者文件
-function removeDs (parentNode,title) {
+function removeDs (parentNode,result) {
+	
 	// 删除功能
 	var dels = $('.del');
-	
-	var arr = [];
-	// 浅拷贝数组
-	for (var j = 0; j < dels.length; j++) {
-		arr[j] = dels[j];
-		arr[j].onclick = function (e) {
+	for (var i = 0; i < dels.length; i++) {
+		
+		dels[i].onclick = function (e) {
 			var that = this;
-			var child = parentNode.childNodes;
-			// 使用数组的indexOf方法，移除正确对应的节点
-			parentNode.removeChild(child[arr.indexOf(this)]);
-			for (var i = 0; i < midList.children.length; i++) {
-				if (midList.children[i].className === 'task' + title) {
-					
-					// 删除富文本区  rightList
-					for (var j = 0; j < midList.children[i].children.length; j++) {
-						var target = $('.fwb' + midList.children[i].children[j].innerHTML)[0];
-						rightList.removeChild(target);
-					}
-
-					// 删除midlist
-					midList.removeChild(midList.children[i]);
-					
+			// ==============================================================
+			// =============================目录层删除逻辑=====================
+			// 将leftList中的dt , dd 联系起来
+			var _dl = [], 
+					dt = [], 
+					dd = [];
+			for (var j = 0; j < parentNode.children.length; j++) {
+				_dl.push(parentNode.children[j]);
+				if(parentNode.children[j].nodeName.toLowerCase() === 'dt'){
+					dt.push(parentNode.children[j]);
+				}else{
+					dd.push(parentNode.children[j]);
 				}
 			}
-			// var target = $('.task' + that.parentNode.className)[0];
-			// // console.log(target)	
-			// if (target.children.length !== 0) {
-			// 	for (var k = 0; k < target.children.length; k++) {
-			// 		if (target.children[k] !== 0) {
-			// 			var child = $('.fwb' + target.children[k].innerHTML)[0];
-			// 			rightList.removeChild(child);
-			// 		}
-			// 	}
-			// }
 			
-			// 防止冒泡导致增加目录或者文件
+			// remove  dt  移除目录节点
+			if(that.parentNode.nodeName.toLowerCase() === 'dt'){
+
+				var dl_index = _dl.indexOf(that.parentNode);
+				var dt_index = dt.indexOf(that.parentNode);
+				// 当仅有一个目录或者目录是最后一个时，移除目录及其文件
+				if(dt_index === (dt.length - 1)){
+					// 子节点移除次数判定
+					for (var k = dl_index - parentNode.children.length; k < parentNode.children.length - dl_index; k++) {
+						if (k <= parentNode.children.length - dl_index ) {
+							// 每次移除节点的index位置始终是移除X 的位置
+							parentNode.removeChild(parentNode.children[dl_index]); 
+						}
+					}
+				}else{
+					// 在dl 中的 index
+					var next_dt_index = _dl.indexOf(dt[dt_index + 1]);
+					for (var f = dl_index ; f < next_dt_index; f++) {
+						if (f <= next_dt_index) {
+							// 每次移除节点的index位置始终是移除X 的位置
+							parentNode.removeChild(parentNode.children[dl_index]);
+						}
+					}
+				}
+				// parentNode.removeChild(that.parentNode);
+			// 当点击的是dd  文件时，	仅删除当前dd即可	
+			}else{
+				parentNode.removeChild(that.parentNode)
+			}
+		// ===================================================================
+		// midlist && rightlist 同时删除
+			for (var m = 0; m < midList.children.length; m++) {
+				if (midList.children[m].className === result) {
+					midList.removeChild(midList.children[m])
+				}
+			}
+
+			for (var n = 0; n < rightList.children.length; n++) {
+				if (rightList.children[n].className === result) {
+					rightList.removeChild(rightList.children[n])
+				}
+			}
 			e.stopPropagation();
 		}
 	}
 };
 
+function overShowDel(parentNode) {
+	for (var i = 0; i < parentNode.children.length; i++) {
+		parentNode.children[i].onmouseover = function () {
 
-function addDiv (parentNode,targetNode,ID,claName) {
-	// midlist 下的子元素查重
-	var child =targetNode.children;
-		for (var i = 0; i < child.length; i++) {
-			if(child[i].className === (claName + ID)){
-				return;
-			}
+			this.children[1].style.display = 'block';
+			this.children[1].className = 'glyphicon glyphicon-plus' + ' i-rotate';
 		}
-	// 均符合才进行创建div
-	var task = document.createElement('div');
-	task.className = claName + ID;
-	targetNode.appendChild(task);
-				
+		parentNode.children[i].onmouseout = function () {
+			this.children[1].style.display = 'none';
+			this.children[1].className = 'glyphicon glyphicon-plus';
+		}
+	}
+}
+// 对midList && rightList 增加对应的div
+function addDiv (targetNode,result) {
+	var div = document.createElement('div');
+	div.className = result;
+	targetNode.appendChild(div);
 	// toggleFwb(ID)			
-	return task;
+	return div;
 }
 
+function showDiv(parentNode,result,start) {
+	for (var i = start; i < parentNode.children.length; i++) {
+		if(parentNode.children[i].className === result){
+			parentNode.children[i].style.display = 'block';
+		}else{
+			parentNode.children[i].style.display = 'none';
+		}
+	}
+}
+
+// 找到文件对应的div
+function clickShowDiv(targetNode,target,parentNode,start) {
+	for (var i = 0; i < targetNode.children.length; i++) {
+		if (targetNode.children[i].nodeName.toLowerCase() === target){
+			targetNode.children[i].onclick = function (e) {
+				// 根据dd的className获取task下的div
+				var id = this.className;
+	
+				showDiv(parentNode,id,start);
+				
+				e.stopPropagation();
+
+			}
+		}
+	}
+}
+
+
+
+function getDiv(parentNode,start) {
+	for (var i = start; i < parentNode.children.length; i++) {
+		if(parentNode.children[i].style.display === 'block'){
+			return parentNode.children[i];
+		}
+	}
+}
+
+// =======================================add2 click 点击
+// 新增任务弹框逻辑及内容
+function mkMidCon(parentNode) {
+	var p = document.createElement('p');
+	var result = prompt();
+	// 限制相同输出
+	for (var i = 0; i < parentNode.children.length ;i++) {
+		if(parentNode.children[i].nodeName.toLowerCase() === 'p' && parentNode.children[i].innerHTML === result){
+			return;
+		}
+	}
+	if (result !== ''&& result !== null) {
+		p.className = result;
+		p.innerHTML = result;
+		parentNode.appendChild(p);
+		return result;
+	}
+}
+function mkMidDate(parentNode) {
+	var h3 = document.createElement('h3');
+	parentNode.appendChild(h3);
+	return h3;
+}
+
+// 新增富文本选区---------rightList div
+// 已经采用addDiv() className === 'fwb' + result
+// 新增富文本选区---------标题
+function mktit(parentNode,result) {
+	var p = document.createElement('p');
+	p.innerHTML = result;
+	parentNode.appendChild(p);
+}
+// 新增富文本选区---------富文本
+function mkfwd(parentNode) {
+	var div = document.createElement('div');
+	div.className = 'contentEditable';
+	div.contentEditable = true;
+	parentNode.appendChild(div);
+	return div;
+}
+
+// 任务日期
+function addDate(parentNode){
+	var label = document.createElement('label');
+	var input = document.createElement('input');
+	input.size = '16';
+	input.type = 'text';
+	input.className = 'form_datetime';
+	label.innerHTML = '任务日期 : ';
+	parentNode.appendChild(label);
+	parentNode.appendChild(input);
+	return input;
+}
 
 
 
@@ -352,50 +376,14 @@ function insertAfter(newElement,target) {
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// function $(name) {
-// 	if (name.indexOf('#') > -1) {
-// 		return document.getElementById(name);
-// 	}else if(name.indexOf('.') > -1){
-// 		var result = name.slice(1);
-// 		return document.getElementsByClassName(result);
-// 	}
-// }; 
-
-
-
-
-
-
-
-
+function $(name) {
+	if (name.indexOf('#') > -1) {
+		return document.getElementById(name);
+	}else if(name.indexOf('.') > -1){
+		var result = name.slice(1);
+		return document.getElementsByClassName(result);
+	}
+} 
 
 
 // 类似于prototype 封装函数的精髓  JavaScript类库本质的探索
--- todo/task.js
+++ todo/task.js
@@ -16,7 +16,7 @@ $.fn.fixHeight = function (options) {
 $.fn.fixHeight.defaults = {
 	reduceHeight : 40 
 }
-
+// start=========================
 var $leftList = $('.left-list')
 var $midList = $('.mid-list')
 
@@ -31,89 +31,98 @@ var $add1 = $('.add-list a').eq(0);
 var $add2 = $('.add-list a').eq(1);
 var $dl = $('.categories');
 $add1.on('click',function () {
-	mkdir('目录分类 : ',$dl);
+	// 创建目录
+	mkdir('目录分类 : ' , $dl);
+
+	// 在目录分类和文件中遍历，寻找目录，点击产生文件
+	$add2.on('click',function (e) {
+		// 让对应的dd文件，mid div , right div 显示
+		
+		mkfile('文件名称' , $(this));
+
+
+		
+
+
+	})
+
+
 
 })
+
+
 
 // 新建目录
 function mkdir(title,parentNode,childNode) {
 	// result 为promt输入框中的输入值
 	var result = prompt(title);
-	console.log(typeof result)
 	if (result !== ''&& result !== null) {
 		// 创建dt 
-		
-			addDt(result,parentNode);
-		
-
-		// removeDs(parentNode);
+			addDl(result,parentNode);
+		// 移除dt	
+		removeDs();
 	}
 }
-
+// 创建dd
+function mkfile(title,parentNode) {
+	// result 为promt输入框中的输入值
+	var result = prompt(title);
+	if (result !== ''&& result !== null) {
+		// 创建dd 
+			addDd(result,parentNode);
+		// 创建task div
+		addDiv(result,$midList);
+		removeDs();
+	}
+}
 // 创建dt
 function addDt (result,parentNode) {
 	// 避免重复
 	if(parentNode.children().hasClass(result)) return;
 	// 创建目录dt
-	var $dt = $("<dt></dt>");
+	var $dt = $("<dt></dt>").text(result).addClass(result);
 	// 删除键
-	var $span = $("<span></span>");
-	$dt.text(result).addClass(result);
+	var $span = $("<span></span>").text('X').addClass('del');
 
-	$span.text('x').addClass('del');
 	$dt.appendTo(parentNode);
 	$span.appendTo($dt);
 	
 };
+// 创建dd
+function addDd(result,parentNode) {
+	if(parentNode.children().hasClass(result)) return;
+	var $dd = $("<dd></dd>").text(result).addClass(result);
+	var $span = $("<span></span>").text('X').addClass('del');
 
+	$dd.appendTo(parentNode);
+	$span.appendTo($dd);
+}
+// 创建task  div
+function addDiv(result,parentNode) {
+	if(parentNode.children().hasClass(result)) return;
+	var $div = $("<div></div>").addClass(result);
+	$div.appendTo(parentNode);
+}
 // 移除目录或者文件
-function removeDs (parentNode,title) {
+function removeDs (result) {
 	// 删除功能
 	var $dels = $('.del');
-	$dels.on('click',function () {
-		
+	$dels.on('click',function (e) {
+		e.stopPropagation();
+		var claName = e.target.parentNode.className;
+		$('.'+claName).remove();
 	})
-	var arr = [];
-	// 浅拷贝数组
-	for (var j = 0; j < dels.length; j++) {
-		arr[j] = dels[j];
-		arr[j].onclick = function (e) {
-			var that = this;
-			var child = parentNode.childNodes;
-			// 使用数组的indexOf方法，移除正确对应的节点
-			parentNode.removeChild(child[arr.indexOf(this)]);
-			for (var i = 0; i < midList.children.length; i++) {
-				if (midList.children[i].className === 'task' + title) {
-					
-					// 删除富文本区  rightList
-					for (var j = 0; j < midList.children[i].children.length; j++) {
-						var target = $('.fwb' + midList.children[i].children[j].innerHTML)[0];
-						rightList.removeChild(target);
-					}
-
-					// 删除midlist
-					midList.removeChild(midList.children[i]);
-					
-				}
-			}
-			// var target = $('.task' + that.parentNode.className)[0];
-			// // console.log(target)	
-			// if (target.children.length !== 0) {
-			// 	for (var k = 0; k < target.children.length; k++) {
-			// 		if (target.children[k] !== 0) {
-			// 			var child = $('.fwb' + target.children[k].innerHTML)[0];
-			// 			rightList.removeChild(child);
-			// 		}
-			// 	}
-			// }
-			
-			// 防止冒泡导致增加目录或者文件
-			e.stopPropagation();
-		}
-	}
+	
 };
 
-
+// 点击dd，找到对应task div
+function getTask(id) {
+	if($midList.children().hasClass(id)){
+		$midList.children(id).css("display","block");
+	}else{
+		$midList.children().slice(2,$(this).children().length-1).css("display","none");
+	}
+}
 
 
 
